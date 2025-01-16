51f5564f717e4c838d277fd6a461b327ab5219b4
Gergana Dimitrova <geri0547@abv.bg>
2025-01-16 21:04:56 +0200

    Made templates for the levels of the game.

---
+++ DungeonEscapeGame/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+
+const int MAX_SIZE = 100;
+
+void readPlayerBoard(const char* filePath, char matrix[][MAX_SIZE], int rows, int cols)
+{
+	if (!filePath)
+	{
+		return;
+	}
+
+	std::ifstream ifs(filePath);
+
+	if (!ifs.is_open())
+	{
+		return;
+	}
+
+	for (size_t i = 0; i < rows; i++)
+	{
+		for (size_t j = 0; j < cols; j++)
+		{
+			ifs >> matrix[i][j];
+		}
+	}
+
+	ifs.close();
+}
+
+void printMatrix(char matrix[][MAX_SIZE], int rows, int cols)
+{
+	for (size_t i = 0; i < rows; i++)
+	{
+		for (size_t j = 0; j < cols; j++)
+		{
+			if (matrix[i][j] == ' ')
+			{
+				std::cout << " ";
+			}
+			else
+			{
+				std::cout << matrix[i][j];
+			}
+
+
+		}
+		std::cout << std::endl;
+	}
+}
+
+
+int main()
+{
+	char matrix[MAX_SIZE][MAX_SIZE]{};
+
+	readPlayerBoard("Level1.1.txt", matrix, 15, 15);
+
+	printMatrix(matrix, 15, 15);
+
+	return 0;
+}
